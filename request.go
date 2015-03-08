718c65bec3c54b66f01181e3bd798b06424de122
Yuchen Wang <yulon@me.com>
2015-03-08 15:47:18 +0800

    Improved ReadRequest

--- request.go
+++ request.go
@@ -2,6 +2,10 @@ package httpoi
 
 import (
 	"io"
+	"errors"
+	"bytes"
+	"net"
+	//"fmt"
 )
 
 type RequestLine struct{
@@ -18,4 +22,88 @@ type RequestHeader struct{
 type RequestR struct{
 	*RequestHeader
 	io.Reader
+}
+
+func ReadRequest(conn net.Conn) (rq *RequestR, err error) {
+	buf := bytes.NewBuffer(make([]byte, 0, 128))
+
+	// Read Request Line
+	rqLine := &RequestLine{}
+	rqLineTokens := make([]string, 2, 2)
+	rqLineNowToken := 0
+	b := make([]byte, 1, 1)
+	for {
+		conn.Read(b)
+		switch b[0] {
+			case ' ':
+				rqLineTokens[rqLineNowToken] = buf.String()
+				buf.Reset()
+				rqLineNowToken++
+
+			case '\r':
+				conn.Read(b)
+				if b[0] == '\n' {
+					if rqLineNowToken == 2 {
+						rqLine.HTTPVersion = buf.String()
+						buf.Reset()
+						if rqLine.HTTPVersion == "HTTP/1.1" {
+							rqLine.Method = rqLineTokens[0]
+							rqLine.URI = rqLineTokens[1]
+
+							// Read Request Header Fields
+							rqHF := HeaderFields{}
+							rqHFName := ""
+							rqHFColon := false
+							for {
+								conn.Read(b)
+								switch b[0] {
+									case ':':
+										if !rqHFColon {
+											rqHFColon = true
+											rqHFName = buf.String()
+											buf.Reset()
+										}
+
+									case '\r':
+										conn.Read(b)
+										if b[0] == '\n' {
+											if rqHFName != "" {
+												rqHF[rqHFName] = buf.String()
+												buf.Reset()
+												rqHFName = ""
+												rqHFColon = false
+											}else{
+												rq = &RequestR{
+													RequestHeader: &RequestHeader{
+														RequestLine: rqLine,
+														HF: rqHF,
+													},
+													Reader: conn,
+												}
+												return
+											}
+											
+										}else{
+											buf.Write(b)
+										}
+
+									default:
+										buf.Write(b)
+								}
+							}
+						}else{
+							err = errors.New("Protocol is not supported")
+						}
+					}else{
+						err = errors.New("Request-Line token count mismatch")
+					}
+					return
+				}else{
+					buf.Write(b)
+				}
+
+			default:
+				buf.Write(b)
+		}
+	}
 }
--- server.go
+++ server.go
@@ -28,93 +28,23 @@ func Sever(laddr string, h SeverHandler) error {
 var lang = runtime.Version()
 
 func saw(conn net.Conn, h SeverHandler) {
-	rawReq := make([]byte, 512)
-	rawReqLen, err := conn.Read(rawReq)
+	rq, err := ReadRequest(conn)
 	if err == nil {
-		rqLine := &RequestLine{}
-
-		// Parse Request Line
-		i, sp1, sp2 := 0, 0, 0
-		prl:
-		for ; i < rawReqLen; i++ {
-			switch rawReq[i] {
-				case ' ':
-					if sp1 == 0 {
-						sp1 = i
-					}else{
-						sp2 = i
-					}
-
-				case '\r':
-					rqLine.HTTPVersion = string(rawReq[sp2+1:i])
-
-				case '\n':
-					break prl
-			}
-		}
-
-		if rqLine.HTTPVersion == "HTTP/1.1" { // Is HTTP
-			rqLine.Method = string(rawReq[:sp1])
-			rqLine.URI = string(rawReq[sp1+1:sp2])
-
-			// Parse Request Header HF
-			rqHF := HeaderFields{}
-
-			prh:
-			for keyStart, keyEnd, valStart := i + 1, 0, 0; i < rawReqLen; i++ {
-				switch rawReq[i] {
-					case ':':
-						if keyEnd == 0 {
-							keyEnd = i
-							for y := 1; i+1 < rawReqLen; y++ {
-								if rawReq[i+y] != ' ' && rawReq[i+y] != '\t' {
-									i = i + y - 1
-									valStart = i + 1
-									continue prh
-								}
-							}
-						}
-
-					case '\r':
-						if keyEnd - keyStart > 0 {
-							for y := 1; i+1 < rawReqLen; y++ {
-								if rawReq[i-y] != ' ' && rawReq[i-y] != '\t' {
-									rqHF[string(rawReq[keyStart:keyEnd])] = string(rawReq[valStart:i-y+1])
-									continue prh
-								}
-							}
-						}
-
-					case '\n':
-						keyStart = i + 1
-						keyEnd = 0
-				}
-			}
-
-			rq := &RequestR{
-				RequestHeader: &RequestHeader{
-					RequestLine: rqLine,
-					HF: rqHF,
+		rs := &ResponseW{
+			ResponseHeader: &ResponseHeader{
+				StatusLine: &StatusLine{
+					HTTPVersion: rq.HTTPVersion,
 				},
-			}
-
-			rs := &ResponseW{
-				ResponseHeader: &ResponseHeader{
-					StatusLine: &StatusLine{
-						HTTPVersion: rqLine.HTTPVersion,
-					},
-					HF: HeaderFields{
-						"Date": time.Now().Format(time.RFC1123),
-						"Server": "HTTPOI",
-						"X-Powered-By": lang,
-					},
+				HF: HeaderFields{
+					"Date": time.Now().Format(time.RFC1123),
+					"Server": "HTTPOI",
+					"X-Powered-By": lang,
 				},
-				wc: &writeNopCloser{conn},
-			}
-
-			h(rs, rq)
-			rs.wc.Close()
+			},
+			wc: &writeNopCloser{conn},
 		}
+		h(rs, rq)
+		rs.wc.Close()
 	}
 	conn.Close()
 }
